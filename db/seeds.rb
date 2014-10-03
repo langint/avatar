0ee4491168ff0d49fbeb5f40d003196b2558c372
Mark Kit <mark.kit@langint.com>
2014-10-03 16:14:17 -0500

    Updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
-Users.create([{name: 'John', avatar:'rosebud.gif', time_updated: '2014-10-03 18:24:37'},{name: 'Mary', avatar:'bloam.jpg', time_updated: '2014-10-03 16:24:22'},{name: 'Bob', avatar:'lev62.gif', time_updated: '2014-10-03 02:45:35'},{name: 'Silvester', avatar:'mahashi2.jpg', time_updated: '2014-10-03 12:21:27'},{name: 'George', avatar:'', time_updated: '2014-10-03 11:24:37'}])
+User.create([{name: 'John', avatar:'rosebud.gif', time_updated: '2014-10-03 18:24:37'},{name: 'Mary', avatar:'bloam.jpg', time_updated: '2014-10-03 16:24:22'},{name: 'Bob', avatar:'lev62.gif', time_updated: '2014-10-03 02:45:35'},{name: 'Silvester', avatar:'mahashi2.jpg', time_updated: '2014-10-03 12:21:27'},{name: 'George', avatar:'', time_updated: '2014-10-03 11:24:37'}])
 
