a4f39adef95cdebf2a15988243e0bb016421a559
Mark Kit <mark.kit@langint.com>
2014-10-03 18:25:51 -0500

    Fixed some code formatting issues

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -5,22 +5,22 @@ class AvatarsController < ApplicationController
   
   def show
     @userid=params[:userid].to_i if !params[:userid].nil?
-	@users=User.all.pluck(:id)
-	return :back if @userid.nil?
+    @users=User.all.pluck(:id)
+    return :back if @userid.nil?
 	
-	@img=User.find_by_id(@userid)[:avatar]
-	@img.nil?||@img.empty? ? @avatar=READ_PATH+"blank.jpg" : @avatar=READ_PATH+@img	
+    @img=User.find_by_id(@userid)[:avatar]
+    @img.nil?||@img.empty? ? @avatar=READ_PATH+"blank.jpg" : @avatar=READ_PATH+@img	
   end
 
   def create
     @userid=params[:userid]
-	uploaded=params[:picture]
-	if !uploaded.nil?
-	  filename= uploaded.original_filename
-	  Avatarimage.save(uploaded) 
-	  User.register(@userid,filename)
-	end
-	redirect_to :action=>'show',:userid=>@userid
+    uploaded=params[:picture]
+    if !uploaded.nil?
+       filename= uploaded.original_filename
+       Avatarimage.save(uploaded) 
+       User.register(@userid,filename)
+    end
+    redirect_to :action=>'show',:userid=>@userid
   end
  
   def destroy
