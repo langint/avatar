eb4928d8cf2ca63dff296503b9be003a08823761
Mark Kit <mark.kit@langint.com>
2014-10-03 16:46:47 -0500

    DB update

--- db/migrate/20141002220851_create_users.rb
+++ db/migrate/20141002220851_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
-      t.timestamps
-    end
+      t.string :first_name, limit: 30
+      t.string :avatar, limit: 32
+      t.time :time_updated
+     end
   end
 end
