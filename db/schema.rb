7e5c44e77b7086f716505870302e40966b0bfc91
Mark Kit <mark.kit@langint.com>
2014-10-03 15:53:11 -0500

    DB cleaned up

--- db/schema.rb
+++ db/schema.rb
@@ -11,36 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002204036) do
-
-  create_table "avatar", force: true do |t|
-    t.string   "path",    limit: 128
-    t.datetime "time"
-    t.integer  "userid"
-    t.boolean  "active"
-    t.integer  "user_id"
-  end
-
-  add_index "avatar", ["userid", "active"], name: "user", using: :btree
-
-  create_table "avatarimage", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "images", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "user", force: true do |t|
-    t.string  "first_name", limit: 30
-    t.integer "avatar"
-  end
+ActiveRecord::Schema.define(version: 20141002233830) do
 
   create_table "users", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "first_name",   limit: 30
+    t.string   "avatar",       limit: 32
+    t.datetime "time_updated"
   end
 
 end
