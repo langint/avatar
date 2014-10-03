ddce33da53eb0aac0a747646406c2f27a2e4a8e4
Mark Kit <mark.kit@langint.com>
2014-10-03 18:41:20 -0500

    Code formatting improved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-attr_accessible :avatar, :time_updated, :first_name
+  attr_accessible :avatar, :time_updated, :first_name
 
   def self.register(user,avatar)
-	self.update(user,:avatar=>avatar, :time_updated=>Time.now)
+    self.update(user,:avatar=>avatar, :time_updated=>Time.now)
   end
 
 end
