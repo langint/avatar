01a26e296a9aac02fb4dd4c15d3537e5cacf58b5
Mark Kit <mark.kit@langint.com>
2014-10-03 15:58:32 -0500

    DB cleanup

--- app/models/imagepointer.rb
+++
@@ -1,2 +0,0 @@
-class Imagepointer < ActiveRecord::Base
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+# This file contains all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Users.create([{name: 'John', avatar:'rosebud.gif', time_updated: '2014-10-03 18:24:37'},{name: 'Mary', avatar:'bloam.jpg', time_updated: '2014-10-03 16:24:22'},{name: 'Bob', avatar:'lev62.gif', time_updated: '2014-10-03 02:45:35'},{name: 'Silvester', avatar:'mahashi2.jpg', time_updated: '2014-10-03 12:21:27'},{name: 'George', avatar:'', time_updated: '2014-10-03 11:24:37'}])
+
