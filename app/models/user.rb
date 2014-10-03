a36656468c03518817b3c585397c175c6f5ca884
Mark Kit <mark.kit@langint.com>
2014-10-03 17:44:45 -0500

    Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-attr_accessible :avatar, :time_updated
+attr_accessible :avatar, :time_updated, :first_name
 
   def self.register(user,avatar)
 	self.update(user,:avatar=>avatar, :time_updated=>Time.now)
